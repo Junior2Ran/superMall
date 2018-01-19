aa403d783c9461a7aa1a09200726640ac4e010f5
Junior2Ran <hedongran@baidu.com>
2018-01-19 13:14:30 +0800

    combine js

--- src/page/mobile/payment/index.jsx
+++ src/page/mobile/payment/index.jsx
@@ -20,14 +20,7 @@ export default class Payment extends React.Component {
     }
 
     componentWillMount() {
-        wx.config({
-            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-            appId: 'wx6d6fd71af24c22c3', // 必填，公众号的唯一标识
-            timestamp: , // 必填，生成签名的时间戳
-            nonceStr: '', // 必填，生成签名的随机串
-            signature: '',// 必填，签名，见附录1
-            jsApiList: ['chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-        });
+        
     }
 
     showModal() {
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name]-[hash:6].js'
+    filename: './js/[name]-[hash:6].js'
   },
   module: {
     rules: [
