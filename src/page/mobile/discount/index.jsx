16eab4ab9369cf72ef2481a72cf357075d5b9009
Junior2Ran <hdr01@126.com>
2018-01-23 14:52:10 +0800

    change discount page

--- src/page/mobile/discount/index.jsx
+++ src/page/mobile/discount/index.jsx
@@ -15,7 +15,7 @@ export default class Discount extends React.Component {
 
     render() {
         return <Layout header={false} footer={false}>
-            <List renderHeader={() => '我的优惠券'}>
+            <List renderHeader={() => '未使用'}>
                 <Item multipleLine onClick={() => {}} extra="2018-07-01到期">
                     ￥500 <Brief>家电类满10000可用</Brief>
                 </Item>
@@ -23,6 +23,19 @@ export default class Discount extends React.Component {
                     ￥500 <Brief>家电类满10000可用</Brief>
                 </Item>
                 <Item multipleLine onClick={() => {}} extra="2018-07-01到期">
+                    ￥500 <Brief>家电类满10000可用</Brief>
+                </Item>
+            </List>
+            <List renderHeader={() => '已使用'}>
+                <Item multipleLine disabled extra="2018-07-01到期">
+                    ￥500 <Brief>家电类满10000可用</Brief>
+                </Item>
+            </List>
+            <List renderHeader={() => '已过期'}>
+                <Item multipleLine disabled extra="2018-07-01到期">
+                    ￥500 <Brief>家电类满10000可用</Brief>
+                </Item>
+                <Item multipleLine disabled extra="2018-07-01到期">
                     ￥500 <Brief>家电类满10000可用</Brief>
                 </Item>
             </List>
